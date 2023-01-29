89221ed1bd7fe621fd94dfd3aed1df56b91c4c60
Shubham-Hazra <shubhamhazra1234@gmail.com>
2023-01-30 00:16:44 +0530

    Added comments

--- src/message.py
+++ src/message.py
@@ -2,21 +2,25 @@ import threading
 import time
 
 
+# Class for message
 class Message:
     def __init__(self, sender, message, is_block):
         self.sender = sender
-        self.is_block = is_block
+        self.is_block = is_block  # True if message is block, False if message is transaction
         self.message = message
         self.time = time.time()
 
+    # Function for printing message information
     def print(self):
         print("Message from " + str(self.sender) + " at time " +
               str(self.time) + " with message " + str(self.message))
 
+    # Function for sending message
     def send(self, receiver_function, sleep_time):
         def send_message():
-            time.sleep(sleep_time)
+            time.sleep(sleep_time)  # Sleep for sleep_time seconds
             receiver_function(self)
+        # Create thread i.e. Call send_message function
         t = threading.Thread(target=send_message)
         t.start()
 
