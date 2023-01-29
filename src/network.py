633cb5758f9d3de7c29017246737b5312a18d994
AtharvaTambat <tambat.atharva@gmail.com>
2023-01-29 14:28:00 +0530

    Updating script to create graph

---
+++ src/network.py
@@ -0,0 +1,45 @@
+import networkx as nx
+import random 
+import matplotlib.pyplot as plt
+
+class Network:
+    def __init__(self, num_nodes):
+        self.num_nodes = num_nodes; # Number of nodes in theconnected graph
+        self.G = nx.Graph() # Stores the graph of the network
+        self.create_graph() # create the P2P Network
+       
+    
+    def create_graph(self):
+        self.G.add_nodes_from([i for i in range(self.num_nodes)])
+
+        sequence = [random.randint(4,8) for i in range(self.num_nodes)] # Each node has 4 to 8 peers
+        # Generate a valid degree sequence
+        while not nx.is_graphical(sequence, "hh"): # Check if the given degree sequence is valid 
+            sequence = [random.randint(4,8) for i in range(self.num_nodes)]
+
+        # Sort the degree sequence
+        sequence.sort()
+        curr_deg = [0 for i in range(self.num_nodes)] # Maintains the current degree of all the nodes
+
+        for i in range(self.num_nodes-1, -1, -1):
+            for j in range(i-1, -1, -1):
+                if curr_deg[i] == sequence[i]:
+                    break
+                elif curr_deg[j] < sequence[j]:
+                    self.G.add_edge(i,j)
+                    curr_deg[i]+=1
+                    curr_deg[j]+=1
+
+        # Print the degree sequence
+        print(sequence)
+
+        # Print the graph
+        nx.draw(self.G, with_labels=True)
+        plt.show()
+
+
+# Testing the class
+N = Network(20)
+
+
+
