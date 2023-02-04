b63c0c9884128bdedc37cedc001966c7aff01c55
Shubham-Hazra <shubhamhazra1234@gmail.com>
2023-02-04 23:34:33 +0530

    block structure updated

--- src/block.py
+++ src/block.py
@@ -1,13 +1,13 @@
 # Class for block
 class Block:
     _id = 0 # Global block ID
-    def __init__(self,creator_id, previous_block_id,create_time, transactions,block_id = None,length = 0): # length - stores the length of the block from the genesis block in the 
+    def __init__(self,creator_id, previous_block_id,created_at, transactions,block_id = None,length = 0): # length - stores the length of the block from the genesis block in the 
         Block._id += 1 # Increment the global block ID
         block_id = Block._id 
-        self.id  =  f"Block_{block_id}" # Set the ID of the block
+        self.id  =  f"Block_{block_id}" # Set the ID of the block automatically
         self.creator_id = creator_id # Set the ID of the creator of the block
         self.previous_id = previous_block_id # Set the ID of the previous block
-        self.create_time = create_time # Set the time of creation of the block
+        self.created_at = created_at # Set the time of creation of the block
         self.transactions = transactions # List of all the TXNs
         self.length = length # Length of the block initially set to 0
 
